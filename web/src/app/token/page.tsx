292c742a3f49dfd5bb54c8628c61a3c15a4bd561
sfshf <sfshf@github.com>
2024-06-12 21:52:46 +0800

    update token tab

--- web/src/app/token/page.tsx
+++ web/src/app/token/page.tsx
@@ -11,6 +11,15 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import shared from "../shared";
+import { request, IncomingMessage, RequestOptions } from "http";
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import moment from "moment";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   props,
@@ -19,78 +28,227 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+interface Column {
+  id: string;
+  label: string;
+  minWidth?: number;
+  align?: "right";
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: "symbol", label: "Symbol", minWidth: 100 },
+  { id: "price", label: "Price", minWidth: 100 },
+];
+
 export default function Home() {
+  const [loading, setLoading] = React.useState(false);
   const [toast, setToast] = React.useState("");
   const [errorToast, setErrorToast] = React.useState("");
-  const baseURL = "https://api.binance.com";
+
+  const post = async (
+    path: string,
+    reqData?: any,
+    successAction?: (respData: any) => void
+  ) => {
+    setLoading(true);
+
+    let postData;
+    if (reqData) {
+      postData = JSON.stringify(reqData);
+    }
+    try {
+      let reqOpts: RequestOptions = {
+        path: shared.kongAddress + path,
+        method: "POST",
+      };
+      if (postData) {
+        reqOpts.headers = {
+          "Content-Type": "application/json",
+          "Content-Length": Buffer.byteLength(postData),
+        };
+      } else {
+        reqOpts.headers = {
+          "Content-Type": "application/json",
+        };
+      }
+      const req = request(reqOpts, (res) => {
+        res.setEncoding("utf8");
+        res.on("data", (chunk) => {
+          const respData = JSON.parse(chunk);
+          if (respData.code !== 0) {
+            setErrorToast(respData.debugMessage);
+            return;
+          }
+          if (successAction) {
+            successAction(respData);
+          }
+          setToast("success");
+        });
+      });
+      req.on("error", (e) => {
+        throw e;
+      });
+      if (postData) {
+        req.write(postData);
+      }
+      req.end();
+
+      setLoading(false);
+    } catch (error) {
+      const message = (error as Error).message;
+      setLoading(false);
+      setErrorToast(message);
+    }
+  };
+
+  const [baseCoin, setBaseCoin] = React.useState('USDT');
+  const handleBaseCoinChange = (event: SelectChangeEvent) => {
+    setBaseCoin(event.target.value as string);
+  };
+  const baseCoinList = ["USD", "USDT"]
 
   const [tokens, setTokens] = React.useState<string[]>([
-    "BTCUSDT",
-    "ETHUSDT",
-    "BNBUSDT",
+    "BTC",
+    "ETH",
+    "BNB",
   ]);
-  const [avgPrice, setAvgPrice] = React.useState<any | null>(null);
+  const list = React.useRef<any[]>([]);
 
-  // get symbol info list: curl https://api.binance.com/api/v3/exchangeInfo
   const doRefresh = async () => {
-    let avgPrices: any = {};
+    let localList: any[] = [];
     for (let i = 0; i < tokens.length; i++) {
-      const res = await fetch(
-        `${baseURL}/api/v3/avgPrice?` +
-          new URLSearchParams({ symbol: tokens[i] }).toString()
-      );
-      const data = await res.json();
-      avgPrices[tokens[i]] = data;
+      post(
+        '/doom/getLatestSpotPrice/v1', 
+        {
+          symbol:tokens[i],
+          baseCoin: baseCoin,
+        },
+        (respData)=>{
+          if (respData.code != 0) {
+            setErrorToast(respData.message);
+            return
+          }
+          localList.push({
+            symbol: tokens[i]+"/"+baseCoin,
+            price: respData.data.price,
+          });
+        }
+      )
     }
-    setAvgPrice(avgPrices);
+    list.current = localList;
   };
+
+  useEffect(()=>{
+    doRefresh();
+  }, []);
 
   return (
     <main>
-      <Stack marginX="200px" marginTop="50px" spacing={2}>
-        <Stack direction="row" textAlign="center" justifyContent="left">
+      <Stack
+        spacing={5}
+        direction="column"
+        alignItems="center"
+        sx={{
+          padding: "1rem",
+        }}
+      >
+        <Stack
+          spacing={2}
+          alignItems="center"
+          sx={{
+            padding: "1rem",
+            width: "100%",
+          }}
+          direction="row"
+        >
           <Button
             variant="contained"
-            size="large"
-            fullWidth
+            sx={{ height: "50px", width: "100px", margin: "5px" }}
             onClick={() => {
               doRefresh();
             }}
           >
             Refresh
           </Button>
+          <FormControl 
+            sx={{
+              width: "15%",
+            }}
+          >
+            <InputLabel id="demo-simple-select-label">Base Coin</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={baseCoin}
+              label="Base Coin"
+              onChange={handleBaseCoinChange}
+            >
+              {baseCoinList && baseCoinList.map((item)=>(
+                <MenuItem value={item} key={item}>{item}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Stack>
-        <Stack direction="row" textAlign="center" justifyContent="left">
+        <Stack
+          spacing={2}
+          alignItems="center"
+          sx={{
+            padding: "1rem",
+            width: "100%",
+          }}
+          direction="row"
+        >
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell>Token</TableCell>
-                  <TableCell align="right">Mins</TableCell>
-                  <TableCell align="right">Price</TableCell>
-                  <TableCell align="right">CloseTime</TableCell>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
                 </TableRow>
               </TableHead>
               <TableBody>
-                {tokens &&
-                  avgPrice &&
-                  tokens.map((row) => (
-                    <TableRow
-                      key={row}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {row}
-                      </TableCell>
-                      <TableCell align="right">{avgPrice[row].mins}</TableCell>
-                      <TableCell align="right">
-                        ${avgPrice[row].price}
-                      </TableCell>
-                      <TableCell align="right">
-                        {new Date(avgPrice[row].closeTime).toLocaleString()}
-                      </TableCell>
+              {list &&
+                list.current.map((row) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                      {columns.map((column) => {
+                        const value:any = row[column.id];
+                        if (column.id == "createdAt") {
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {moment(value)
+                                .local()
+                                .format("YYYY-MM-DD HH:mm:ss")}
+                            </TableCell>
+                          );
+                        } else if (column.id == "price") {
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              ${value}
+                            </TableCell>
+                          );
+                        } else {
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format !== undefined &&
+                              typeof value === "number"
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          );
+                        }
+                      })}
                     </TableRow>
-                  ))}
+                  );
+                })}
               </TableBody>
             </Table>
           </TableContainer>
@@ -124,6 +282,12 @@ export default function Home() {
           {toast}
         </Alert>
       </Snackbar>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </main>
   );
 }
